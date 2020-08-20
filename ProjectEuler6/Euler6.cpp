9e44b4f4a90e6163183321b184a8c01bf4939504
mymint01 <captainchuenthavorn@gmail.com>
2020-08-20 10:28:30 +0700

    add user interface

--- ProjectEuler6/Euler6.cpp
+++ ProjectEuler6/Euler6.cpp
@@ -1,19 +1,23 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
 int main() {
-	int x, y, sumX, sumY, squareX, squareY;
+	int x, y, sumX, sumY, squareX, squareY,numX,numY;
+	printf("Enter range of the sum of the squares of the ");
+	scanf("%d", &numX);
+	printf("Enter range of the sum of the squares of the ");
+	scanf("%d", &numY);
 	sumX = 0;
 	sumY = 0;
-	for (x = 1;x <= 100;x++) {
+	for (x = 1;x <= numX;x++) {
 		squareX = x * x;
 			sumX += squareX;
-			
 	}
-	
-	for (y = 1;y <= 100;y++) {
+	for (y = 1;y <= numY;y++) {
 		sumY += y;
 		}
 	squareY = sumY * sumY;
-
-	printf("sum x %d - sum y %d = %d", sumX, squareY, squareY-sumX);
+	printf("\nThe sum of the squares : %d", sumX);
+	printf("\nThe sum of the squares : %d", sumY);
+	printf("\n\nDiference between two number is %d - %d = %d", sumX, squareY, squareY-sumX);
 	return 0;
 }
